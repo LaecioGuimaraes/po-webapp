a344e71d56a223bc09bad8ba53dfb5ee12aff940
Laecio <laecio@pop-os.home>
2025-03-23 23:24:29 -0300

    Correção da configuração da porta no Flask para deploy no Render

--- src/app.py
+++ src/app.py
@@ -19,4 +19,5 @@ def optimize():
         return handle_error(e, data)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
+    # app.run(debug=True)
